3415758d550db38f0d492b1367d52599a1e55071
Yukta Mrutunjay Hadrihalli <118595772+mhyukta@users.noreply.github.com>
2026-07-09 23:17:51 +0530

    Add files via upload

---
+++ pg1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first side: "))
+b = int(input("Enter second side: "))
+c = int(input("Enter third side: "))
+
+if a == b and b == c:
+    print("Equilateral Triangle")
+elif a == b or a == c or b == c:
+    print("Isosceles Triangle")
+else:
+    print("Scalene Triangle")
